2693e66a225df2b20b6683cb2360ed7f7f5773e0
Shrikant Yadav <shrikant9907@gmail.com>
2024-09-09 17:18:51 +0530

    day 5 updates

---
+++ day-5-part2/portfolio/js/scripts.js
@@ -0,0 +1,5 @@
+alert('test')
+
+console.log('Error Message', 'Unable to load website.')
+console.error('Error Message', 'Unable to load website.')
+console.warn('Error Message', 'Unable to load website.')
