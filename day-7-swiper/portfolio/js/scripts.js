94276405f62a7bff42530424c509631b135241d0
Shrikant Yadav <shrikant9907@gmail.com>
2024-09-11 14:19:10 +0530

    Swiper JS and Basics of JavaScript

---
+++ day-7-swiper/portfolio/js/scripts.js
@@ -0,0 +1,35 @@
+// $(document).ready(function() {
+//     // SLIDER
+//     var carouselslider = new Swiper('.carousel-slider', {
+//       spaceBetween: 0,
+//       slidesPerView: 3,
+//       centeredSlides: true,
+//       autoplay: {
+//         delay: 9500,
+//         disableOnInteraction: false,
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//         type: 'progressbar',
+//       },
+//       loop: true,
+//       breakpoints: {
+//         1024: {
+//           slidesPerView: 3
+//         },
+//         768: {
+//           slidesPerView: 2
+//         },
+//         640: {
+//           slidesPerView: 1
+//         },
+//         320: {
+//           slidesPerView: 1
+//         }
+//       }
+//     });
+//   });
